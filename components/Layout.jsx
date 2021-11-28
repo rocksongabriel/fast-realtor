51c8ddbb3533e8ecf2060e62c510e90b3d4093b3
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-11-28 10:24:44 +0000

    Modify Layout component to return something

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,14 +2,14 @@ import Head from "next/head";
 import { Box } from "@chakra-ui/react";
 
 const Layout = ({ children }) => {
-  <>
+  return <>
     <Head>
       <title>Fast Realtor</title>
     </Head>
     <Box maxWidth="1280px" m="auto">
       <header>Navbar</header>
 
-      <main>{children}</main>
+      <main>{ children }</main>
 
       <footer>Footer</footer>
     </Box>
