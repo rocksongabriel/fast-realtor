78ed4c8985ad5e274ec57a8263c1418a85e9dc17
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-11-28 07:51:21 +0000

    Add Banner Component

---
+++ pages/index.jsx
@@ -0,0 +1,54 @@
+import Link from "next/link";
+import Image from "next/image";
+import { Flex, Box, Text, Button } from "@chakra-ui/react";
+
+
+const Banner = ({imageUrl, purpose, title1, title2, desc1, desc2, linkName, buttonText}) => (
+  <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
+    <Image src={imageUrl} width={500} height={300} alt="banner" />
+    <Box p="5">
+      <Text color="gray.500" fontSize="sm" fontWeight="medium">{purpose}</Text>
+      <Text fontSize="3xl" fontWeight="bold">{title1}<br />{title2}</Text>
+      <Text fontSize="lg" paddingTop="3" paddingBottom="3" color="gray.700">{desc1}<br />{desc2}</Text>
+      <Button fontSize="xl" backgroundColor="black" color="white" border="none" p="10">
+        <Link href={linkName}>{buttonText}</Link>
+      </Button>
+    </Box>
+  </Flex>
+)
+
+const Home = () => {
+  return <>
+    <Box>
+      <Banner 
+        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
+        purpose="BUY YOUR DREAM HOME"
+        title1="Affordable Dream Homes for "
+        title2="Everybody"
+        desc1="Buy affordable Homes, Cortages, Apartments for "
+        desc2="You and Your Family"
+        linkName="/search?purpose=for-sale"
+        buttonText="Buy a House"
+      />
+      <Flex>
+        {/* Fetch your data and map over them here ... */}
+      </Flex>
+
+      <Banner 
+        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
+        purpose="RENT A HOME"
+        title1="Affordable Dream Homes for "
+        title2="Everybody to Rent"
+        desc1="Looking For a Place to Stay?"
+        desc2="Rent an Apartment, Home, Cortage at fast-realtor"
+        linkName="/search?purpose=for-rent"
+        buttonText="Find a Rental Home"
+      />
+      <Flex>
+        {/* Fetch your data and map over them here ... */}
+      </Flex>
+    </Box>
+  </>
+}
+
+export default Home;
