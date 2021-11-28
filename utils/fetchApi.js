f2636356fb7619a6dbda65492b9c0f05ca7778d0
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-11-28 08:13:49 +0000

    Modify the fetchAPI function

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -3,11 +3,13 @@ import axios from "axios";
 
 export const baseUrl = "https://bayut.p.rapidapi.com"
 
-export const fetchAPI = async () => {
+export const fetchAPI = async (url) => {
   const { data } = await axios.get((url), {
     headers: {
       'x-rapidapi-host': 'bayut.p.rapidapi.com',
       'x-rapidapi-key': 'aacb4cf585msh54c3d3ca435065ap1dc64cjsn9c32a4957a59'
     }
   })
+
+  return data;
 }
